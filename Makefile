395752dc0645ed76cb0be9fbed0e3232bec277a4
Henrique Marlon <henriquemarloncs@gmail.com>
2024-10-16 01:04:47 -0300

    feat: add support for bare metal approach

--- Makefile
+++ Makefile
@@ -1,3 +1,17 @@
+-include .env.develop
+
+START_LOG = @echo "================================================= START OF LOG ==================================================="
+END_LOG = @echo "================================================== END OF LOG ===================================================="
+
 .PHONY: tests
 tests:
 	@go test -p 1 ./... -coverprofile=./coverage.md -v
+
+.PHONY: machine
+machine:
+	$(START_LOG)
+	@docker build \
+		-t machine:latest \
+		-f ./build/Dockerfile .
+	@cartesi build --from-image machine:latest
+	$(END_LOG)
--- cmd/dapp/main.go
+++ cmd/dapp/main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"context"
 	"log/slog"
+	"os"
 
 	"github.com/rollmelette/rollmelette"
 )
 
 func main() {
-	//////////////////////// Setup Application //////////////////////////
+	//////////////////////// Setup DApp /////////////////////////
 	app := NewApp()
 
-	///////////////////////// Rollmelette //////////////////////////
 	ctx := context.Background()
 	opts := rollmelette.NewRunOpts()
-	err := rollmelette.Run(ctx, opts, app)
-	if err != nil {
+	if rollupUrl, isSet := os.LookupEnv("ROLLUP_HTTP_SERVER_URL"); isSet {
+		opts.RollupURL = rollupUrl
+	}
+	if err := rollmelette.Run(ctx, opts, app); err != nil {
 		slog.Error("application error", "error", err)
 	}
 }
